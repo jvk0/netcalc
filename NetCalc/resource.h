1a3624da126c5a9286c1622cf9e0a43e6a32e979
jvk0 <vaclavik.jano@gmail.com>
2020-04-17 21:12:56 +0200

    Added TAB1 UI

--- NetCalc/resource.h
+++ NetCalc/resource.h
@@ -44,6 +44,10 @@
 #define IDC_TAB1_SPIN_PREFIX            1036
 #define IDC_TAB1_GB_SPACE               1037
 #define IDC_TAB1_ST_INFO                1038
+#define IDC_TAB1_LIST_NETS              1039
+#define IDC_TAB1_BTN_SETHOSTS           1040
+#define IDC_BUTTON2                     1042
+#define IDC_TAB1_BTN_CALC               1042
 
 // Next default values for new objects
 // 
@@ -51,7 +55,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        133
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1039
+#define _APS_NEXT_CONTROL_VALUE         1043
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
