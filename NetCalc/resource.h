30ec11726ae2593aab54e9bb828392d9d65571f1
jvk0 <vaclavik.jano@gmail.com>
2020-04-21 21:18:54 +0200

    Minor UI change in HDLG

--- NetCalc/resource.h
+++ NetCalc/resource.h
@@ -59,7 +59,6 @@
 #define IDC_HDLG_BTN_CLEAR              1050
 
 // Next default values for new objects
-// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        137
