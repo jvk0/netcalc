589ade1679b95722fa4a085eda9aea19c160dfaa
jvk0 <vaclavik.jano@gmail.com>
2020-05-14 20:51:35 +0200

    New wildcard tab initial setup

--- NetCalc/CTabWild.cpp
+++ NetCalc/CTabWild.cpp
@@ -1,20 +1,17 @@
-// CTabWild.cpp : implementation file
-//
-
 #include "pch.h"
+
+#include "resource.h"
+
 #include "NetCalc.h"
 #include "CTabWild.h"
-#include "afxdialogex.h"
-
 
 // CTabWild dialog
 
 IMPLEMENT_DYNAMIC(CTabWild, CDialogEx)
 
 CTabWild::CTabWild(CWnd* pParent /*=nullptr*/)
-	: CDialogEx(IDD_OLE_PL_TAB3, pParent)
+    : CDialogEx(IDD_OLE_PL_TAB3, pParent)
 {
-
 }
 
 CTabWild::~CTabWild()
@@ -23,9 +20,29 @@ CTabWild::~CTabWild()
 
 void CTabWild::DoDataExchange(CDataExchange* pDX)
 {
-	CDialogEx::DoDataExchange(pDX);
+    CDialogEx::DoDataExchange(pDX);
 }
 
+void CTabWild::OnOK()
+{
+    // Prevent Enter from closing the dialog  
+    if ((GetKeyState(VK_RETURN) & 0x8000) == 0)
+        CTabWild::OnOK();
+}
+
+void CTabWild::OnCancel()
+{
+    // Prevent Esc from closing the dialog  
+    if ((GetKeyState(VK_ESCAPE) & 0x8000) == 0)
+        CTabWild::OnCancel();
+}
+
+BOOL CTabWild::OnInitDialog()
+{
+    CDialogEx::OnInitDialog();
+
+    return TRUE;
+}
 
 BEGIN_MESSAGE_MAP(CTabWild, CDialogEx)
 END_MESSAGE_MAP()
--- NetCalc/CTabWild.h
+++ NetCalc/CTabWild.h
@@ -2,22 +2,22 @@
 
 
 // CTabWild dialog
-
-class CTabWild : public CDialogEx
-{
-	DECLARE_DYNAMIC(CTabWild)
-
+class CTabWild : public CDialogEx {
+    DECLARE_DYNAMIC(CTabWild)
 public:
-	CTabWild(CWnd* pParent = nullptr);   // standard constructor
-	virtual ~CTabWild();
+    CTabWild(CWnd* pParent = nullptr);   // standard constructor
+    virtual ~CTabWild();
 
-// Dialog Data
-#ifdef AFX_DESIGN_TIME
-	enum { IDD = IDD_OLE_PL_TAB3 };
-#endif
-
+    // Dialog Data
+    #ifdef AFX_DESIGN_TIME
+        enum { IDD = IDD_OLE_PL_TAB3 };
+    #endif
 protected:
-	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+    virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
 
-	DECLARE_MESSAGE_MAP()
+    virtual void OnOK() override;
+    virtual void OnCancel() override;
+
+    virtual BOOL OnInitDialog() override;
+    DECLARE_MESSAGE_MAP()
 };
